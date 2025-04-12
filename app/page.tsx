752d30c52dc96d17fca87de09d8cb37da7261350
vishal09l <iamvishal085@gmail.com>
2025-04-12 13:00:14 +0530

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div>
-    hello world
-      <h1>Welcome to Next.js!</h1>
-      <Button variant="primary">
-        Click Me
-      </Button>
-    </div>
+    <div></div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+'use client';
+import { useState } from 'react';
+import { Sun, Moon } from 'lucide-react';
 
-const Navbar = () => {
+export default function Navbar() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <div>
-      hello world
-    </div>
-  )
-}
+    <div className={isDarkMode ? 'dark' : ''}>
+      <nav className='h-16 bg-white dark:bg-black shadow px-4 py-3 flex items-center justify-between'>
+        <div className='flex items-center gap-3'>
+          <img
+            src={isDarkMode ? '/logo-white.png' : '/logo-black.png'}
+            alt='Logo'
+            className='h-23 object-contain'
+          />
+        </div>
 
-export default Navbar
+
+        <ul className='flex gap-6 text-black dark:text-white font-medium mr-6'>
+          <button
+            onClick={() => setIsDarkMode(!isDarkMode)}
+            className='text-black dark:text-white cursor-pointer'
+          >
+            {isDarkMode ? <Sun size={22} /> : <Moon size={22} />}
+          </button>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>About Us</a>
+          </li>
+          <li>
+            <a href='#'>Search</a>
+          </li>
+          <li>
+            <a href='#'>Profile</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
